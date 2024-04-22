45a0d94d0fea2de2ef1ddbdcc4b5a9d59cfffb95
gikjuri1 <gikjuri@yandex.ru>
2024-04-22 22:43:10 +0300

    Added date into exception

--- src/main/java/ru/Gik/Task5/exception/ControllerExceptionHandler.java
+++ src/main/java/ru/Gik/Task5/exception/ControllerExceptionHandler.java
@@ -5,11 +5,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+
 public class ControllerExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
     public ErrorMessage resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
-        ErrorMessage message = new ErrorMessage(200,"Resource not found","");
+        ErrorMessage message = new ErrorMessage(200, new Date(),"Resource not found","");
         return message;
     }
 }
--- src/main/java/ru/Gik/Task5/exception/ErrorMessage.java
+++ src/main/java/ru/Gik/Task5/exception/ErrorMessage.java
@@ -2,13 +2,13 @@ package ru.Gik.Task5.exception;
 import java.util.Date;
 public class ErrorMessage {
     private int statusCode;
-    //private Date timestamp;
+    private Date timestamp;
     private String message;
     private String description;
 
-    public ErrorMessage(int statusCode, String message, String description) {
+    public ErrorMessage(int statusCode, Date timestamp, String message, String description) {
         this.statusCode = statusCode;
-        //this.timestamp = timestamp;
+        this.timestamp = timestamp;
         this.message = message;
         this.description = description;
     }
@@ -17,9 +17,9 @@ public class ErrorMessage {
         return statusCode;
     }
 
-    /*public Date getTimestamp() {
+    public Date getTimestamp() {
         return timestamp;
-    }*/
+    }
 
     public String getMessage() {
         return message;
